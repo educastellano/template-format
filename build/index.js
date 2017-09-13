39503bfdff70a47b261d2e3a86eb3072fea7f25a
Eduard Castellano <educastellano08@gmail.com>
2017-09-13 10:27:46 +0200

    1.1.0

--- build/index.js
+++ build/index.js
@@ -1,44 +1,50 @@
 'use strict';
 
 function format(string, object) {
+    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
     if (typeof string !== 'string') {
         return string;
     }
-    var matches = void 0;
-    do {
-        matches = string.match(/{(.*?)}/);
-        if (matches) {
-            var attrs = matches[1].split('.');
-            var value = object;
-            var _iteratorNormalCompletion = true;
-            var _didIteratorError = false;
-            var _iteratorError = undefined;
+    var regex = options.regex || /{(.*?)}/g;
+    return string.replace(regex, function () {
+        for (var _len = arguments.length, match = Array(_len), _key = 0; _key < _len; _key++) {
+            match[_key] = arguments[_key];
+        }
 
+        var attrs = match[1].split('.');
+        var value = object;
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+            for (var _iterator = attrs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                var attr = _step.value;
+
+                value = value ? value[attr] : undefined;
+            }
+        } catch (err) {
+            _didIteratorError = true;
+            _iteratorError = err;
+        } finally {
             try {
-                for (var _iterator = attrs[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                    var attr = _step.value;
-
-                    value = value ? value[attr] : '';
+                if (!_iteratorNormalCompletion && _iterator.return) {
+                    _iterator.return();
                 }
-            } catch (err) {
-                _didIteratorError = true;
-                _iteratorError = err;
             } finally {
-                try {
-                    if (!_iteratorNormalCompletion && _iterator.return) {
-                        _iterator.return();
-                    }
-                } finally {
-                    if (_didIteratorError) {
-                        throw _iteratorError;
-                    }
+                if (_didIteratorError) {
+                    throw _iteratorError;
                 }
             }
-
-            string = string.replace(matches[0], value || '');
         }
-    } while (matches);
-    return string;
+
+        if (!value && options.skipUndefined) {
+            return match[0];
+        } else {
+            return value || '';
+        }
+    });
 }
 
 module.exports = format;
