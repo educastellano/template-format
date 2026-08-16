6ccd97eb5e5a39a2b79cfc4272a00eb52a8a5b69
Eduard Castellano <educastellano08@gmail.com>
2026-08-16 23:18:25 +0200

    Remove build step, add linter, formatter and CI (#7)
    * Remove build step, replace tape for brittle, add CI
    
    * Add lunte and prettier
    
    * Format code
    
    * Add files attribute in package.json
    
    * Fix readme inaccuracies
    
    * Fix readme format

--- test.js
+++ test.js
@@ -1,155 +1,151 @@
-var test = require('tape');
-var format = require('./index')
+const test = require('brittle')
+const format = require('./index')
 
 test('it should format strings with different attributes of an object', function (t) {
-    let string = 'Hello {name}, happy {age} bday!'
-    let object = { name: 'Bob', age: 32 }
+  const string = 'Hello {name}, happy {age} bday!'
+  const object = { name: 'Bob', age: 32 }
 
-    let result = format(string, object)
+  const result = format(string, object)
 
-    t.equals(result, 'Hello Bob, happy 32 bday!');
-    t.end();
-});
+  t.is(result, 'Hello Bob, happy 32 bday!')
+})
 
 test('it should format strings with nested attributes', function (t) {
-    let string = 'Hello {bob.name}, happy {bob.age} bday! I call you at {bob.contact.phone}'
-    let object = {
-        bob: {
-            name: 'Bob', 
-            age: 32, 
-            contact: {
-                phone: '978090909'
-            }
-        }
+  const string = 'Hello {bob.name}, happy {bob.age} bday! I call you at {bob.contact.phone}'
+  const object = {
+    bob: {
+      name: 'Bob',
+      age: 32,
+      contact: {
+        phone: '978090909'
+      }
     }
+  }
 
-    let result = format(string, object)
+  const result = format(string, object)
 
-    t.equals(result, 'Hello Bob, happy 32 bday! I call you at 978090909');
-    t.end();
-});
+  t.is(result, 'Hello Bob, happy 32 bday! I call you at 978090909')
+})
 
 test('it should format strings passing arrays', function (t) {
-    let string = 'Hello {0}, happy {1} bday!'
-    let array = ['Bob', 32]
+  const string = 'Hello {0}, happy {1} bday!'
+  const array = ['Bob', 32]
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Hello Bob, happy 32 bday!');
-    t.end();
-});
+  t.is(result, 'Hello Bob, happy 32 bday!')
+})
 
 test('it should format strings passing arrays of objects', function (t) {
-    let string = '{0.name} is {0.age}, {1.name} is {1.age}, {2.name} is {2.age}'
-    let array = [{ name: 'Bob', age: 32 }, { name: 'Mary', age: 30 }, { name: 'Baby', age: 0 }]
+  const string = '{0.name} is {0.age}, {1.name} is {1.age}, {2.name} is {2.age}'
+  const array = [
+    { name: 'Bob', age: 32 },
+    { name: 'Mary', age: 30 },
+    { name: 'Baby', age: 0 }
+  ]
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Bob is 32, Mary is 30, Baby is 0');
-    t.end();
-});
+  t.is(result, 'Bob is 32, Mary is 30, Baby is 0')
+})
 
 test('it should format strings spreading arrays of objects, using $n', function (t) {
-    let string = 'Hello {$n.name}!'
-    let array = [{ name: 'bob' }, { name: 'mary' }, { name: 'julia' }]
+  const string = 'Hello {$n.name}!'
+  const array = [{ name: 'bob' }, { name: 'mary' }, { name: 'julia' }]
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Hello bob,mary,julia!');
-    t.end();
-});
+  t.is(result, 'Hello bob,mary,julia!')
+})
 
 test('it should format strings spreading object:array:object, using $n', function (t) {
-    let string = 'Hello {people.$n.name}!'
-    let array = { people: [{ name: 'bob' }, { name: 'mary' }, { name: 'julia' }] }
+  const string = 'Hello {people.$n.name}!'
+  const array = { people: [{ name: 'bob' }, { name: 'mary' }, { name: 'julia' }] }
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Hello bob,mary,julia!');
-    t.end();
-});
+  t.is(result, 'Hello bob,mary,julia!')
+})
 
 test('it should format strings spreading object:array:object, using $n and numbers as value', function (t) {
-    let string = 'Ages: {people.$n.age}'
-    let array = { people: [{ name: 'bob', age: 22 }, { name: 'mary', age: 30 }, { name: 'baby', age: 0 }] }
+  const string = 'Ages: {people.$n.age}'
+  const array = {
+    people: [
+      { name: 'bob', age: 22 },
+      { name: 'mary', age: 30 },
+      { name: 'baby', age: 0 }
+    ]
+  }
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Ages: 22,30,0');
-    t.end();
-});
+  t.is(result, 'Ages: 22,30,0')
+})
 
 test('it should format strings spreading object:array:object:array, using $n', function (t) {
-    let string = 'Hello {people.$n.names.$n}!'
-    let array = { people: [{ names: ['bob', 'abc', 'def'] }, { names: ['mary', 'ghi', 'jkl'] }] }
+  const string = 'Hello {people.$n.names.$n}!'
+  const array = { people: [{ names: ['bob', 'abc', 'def'] }, { names: ['mary', 'ghi', 'jkl'] }] }
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Hello bob,abc,def,mary,ghi,jkl!');
-    t.end();
-});
+  t.is(result, 'Hello bob,abc,def,mary,ghi,jkl!')
+})
 
 test('it should format strings spreading object:array:object:array:object, using $n', function (t) {
-    let string = 'Hello {people.$n.names.$n.name}!'
-    let array = { people: [
-            { names: [{ name:'bob' }, { name:'abc' }, { name:'def' }] }, 
-            { names: [{ name:'mary' }, { name:'ghi' }, { name:'jkl' }]}
-        ] 
-    }
+  const string = 'Hello {people.$n.names.$n.name}!'
+  const array = {
+    people: [
+      { names: [{ name: 'bob' }, { name: 'abc' }, { name: 'def' }] },
+      { names: [{ name: 'mary' }, { name: 'ghi' }, { name: 'jkl' }] }
+    ]
+  }
 
-    let result = format(string, array)
+  const result = format(string, array)
 
-    t.equals(result, 'Hello bob,abc,def,mary,ghi,jkl!');
-    t.end();
-});
+  t.is(result, 'Hello bob,abc,def,mary,ghi,jkl!')
+})
 
 test('it should format strings using a custom spreadToken', function (t) {
-    let string = 'Hello {people.$$.name}!'
-    let array = { people: [{ name: 'bob' }, { name: 'mary'}, { name: 'julia'}] }
+  const string = 'Hello {people.$$.name}!'
+  const array = { people: [{ name: 'bob' }, { name: 'mary' }, { name: 'julia' }] }
 
-    let result = format(string, array, { spreadToken: '$$' })
+  const result = format(string, array, { spreadToken: '$$' })
 
-    t.equals(result, 'Hello bob,mary,julia!');
-    t.end();
-});
+  t.is(result, 'Hello bob,mary,julia!')
+})
 
 test('it should format strings using a custom spreadSeparator', function (t) {
-    let string = 'Hello {people.$n.name}!'
-    let array = { people: [{ name: 'bob' }, { name: 'mary'}, { name: 'julia'}] }
+  const string = 'Hello {people.$n.name}!'
+  const array = { people: [{ name: 'bob' }, { name: 'mary' }, { name: 'julia' }] }
 
-    let result = format(string, array, { spreadSeparator: ' ' })
+  const result = format(string, array, { spreadSeparator: ' ' })
 
-    t.equals(result, 'Hello bob mary julia!');
-    t.end();
-});
+  t.is(result, 'Hello bob mary julia!')
+})
 
 test('it should *not* skip undefined attributes when *not* using the option skipUndefined', function (t) {
-    let string = 'Hello {name}, happy {age} bday! I call you at {bob.contact.phone}'
-    let object = { name: 'Bob' }
+  const string = 'Hello {name}, happy {age} bday! I call you at {bob.contact.phone}'
+  const object = { name: 'Bob' }
 
-    let result = format(string, object)
+  const result = format(string, object)
 
-    t.equals(result, 'Hello Bob, happy  bday! I call you at ');
-    t.end();
-});
-
+  t.is(result, 'Hello Bob, happy  bday! I call you at ')
+})
 
 test('it should skip undefined attributes when using the option skipUndefined', function (t) {
-    let string = 'Hello {name}, happy {age} bday! I call you at {bob.contact.phone}'
-    let object = { name: 'Bob' }
+  const string = 'Hello {name}, happy {age} bday! I call you at {bob.contact.phone}'
+  const object = { name: 'Bob' }
 
-    let result = format(string, object, { skipUndefined: true })
+  const result = format(string, object, { skipUndefined: true })
 
-    t.equals(result, 'Hello Bob, happy {age} bday! I call you at {bob.contact.phone}');
-    t.end();
-});
+  t.is(result, 'Hello Bob, happy {age} bday! I call you at {bob.contact.phone}')
+})
 
 test('it should format strings with a different format syntax', function (t) {
-    let string = 'Hello {{name}}, happy {{age}} bday!'
-    let object = { name: 'Bob', age: 32 }
+  const string = 'Hello {{name}}, happy {{age}} bday!'
+  const object = { name: 'Bob', age: 32 }
 
-    let result = format(string, object, { regex: /{{(.*?)}}/g })
+  const result = format(string, object, { regex: /{{(.*?)}}/g })
 
-    t.equals(result, 'Hello Bob, happy 32 bday!');
-    t.end();
-});
+  t.is(result, 'Hello Bob, happy 32 bday!')
+})
